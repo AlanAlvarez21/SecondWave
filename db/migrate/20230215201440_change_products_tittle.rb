7bcfb2bb9203dddb6f724e5f86caaad5ce21c161
Alan Daniel Alvarez Castro <alandanielalvarez00000@gmail.com>
2023-05-17 03:43:13 -0500

    fix: removed bad migration

--- db/migrate/20230215201440_change_products_tittle.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class ChangeProductsTittle < ActiveRecord::Migration[7.0]
-  def change
-    change_table :products do |t|
-      t.rename :tittle, :title
-    end
-  end
-end
