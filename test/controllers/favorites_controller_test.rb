ae2678d1d03c13f724fe6c73d4e5f21da40fa58a
Alan Daniel Alvarez Castro <alandanielalvarez00000@gmail.com>
2023-05-20 09:48:08 -0600

    feat: add delete method to favorites

--- test/controllers/favorites_controller_test.rb
+++ test/controllers/favorites_controller_test.rb
@@ -6,6 +6,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   setup do
     login
     @product = products(:snes)
+    @switch = products(:switch)
   end
 
   test 'should create favorite' do
@@ -15,9 +16,17 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   test 'shoul create favorite' do
     assert_difference('Favorite.count', 1) do
-      post favorites_url(product_id: @product.id)
+      post favorite_url(product_id: @product.id)
     end
 
     assert_redirected_to product_path(@product)
+  end
+
+  test 'shoul unfavorite' do
+    assert_difference('Favorite.count', -1) do
+      delete favorite_url(@switch.id)
+    end
+
+    assert_redirected_to product_path(@switch)
   end
 end
